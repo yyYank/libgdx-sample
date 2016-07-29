e015509dc04396c301b9f96f1460ceb40a47f9d0
yy_yank <yy.yank.dev.blog@gmail.com>
2016-07-30 05:54:08 +0900

    initial commit.

---
+++ desktop/src/com/github/yyyank/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.github.yyyank.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.github.yyyank.libGDXGame;
+
+import java.util.stream.IntStream;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new libGDXGame(), config);
+	}
+}
