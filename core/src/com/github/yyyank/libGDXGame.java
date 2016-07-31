6d42057cb3255488405191505413bfbb383bf24d
yy_yank <yy.yank.dev.blog@gmail.com>
2016-07-31 20:44:39 +0900

    change bg color.

--- core/src/com/github/yyyank/libGDXGame.java
+++ core/src/com/github/yyyank/libGDXGame.java
@@ -8,26 +8,26 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class libGDXGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Texture texture;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		texture = new Texture("badlogic.jpg");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(texture, 0, 0);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		texture.dispose();
 	}
 }
