f7252095b9b3172f52b42afa7d406ff68afa7b0d
yy_yank <yy.yank.dev.blog@gmail.com>
2016-08-01 23:22:12 +0900

    practice 3

---
+++ core/src/com/github/yyyank/libGDXGame3.java
@@ -0,0 +1,39 @@
+package com.github.yyyank;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+
+public class libGDXGame3 extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture texture;
+	TextureAtlas atlas;
+	
+	@Override
+	public void create () {
+		FileHandle fh = Gdx.files.internal("001.txt");
+		batch = new SpriteBatch();
+		atlas = new TextureAtlas(fh);
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.setColor(1, 1, 1, 1);
+		TextureAtlas.AtlasRegion region1 = atlas.findRegion("7TZK1gqH_400x400");
+		batch.draw(region1, 0, 0);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		texture.dispose();
+	}
+}
--- desktop/src/com/github/yyyank/desktop/DesktopLauncher.java
+++ desktop/src/com/github/yyyank/desktop/DesktopLauncher.java
@@ -3,12 +3,13 @@ package com.github.yyyank.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.github.yyyank.libGDXGame;
+import com.github.yyyank.libGDXGame3;
 
 import java.util.stream.IntStream;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new libGDXGame(), config);
+		new LwjglApplication(new libGDXGame3(), config);
 	}
 }
