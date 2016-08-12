645a2f6b3709c3888566b62a069f755502fd184e
yy_yank <yy.yank.dev.blog@gmail.com>
2016-08-12 22:19:15 +0900

    ついついKotlinを突っ込んでしまった

--- desktop/src/com/github/yyyank/desktop/DesktopLauncher.java
+++ desktop/src/com/github/yyyank/desktop/DesktopLauncher.java
@@ -3,10 +3,11 @@ package com.github.yyyank.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.github.yyyank.LibGDXGame5;
+import com.github.yyyank.LibGDXGame7;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new LibGDXGame5(), config);
+		new LwjglApplication(new LibGDXGame7(), config);
 	}
 }
